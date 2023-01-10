f803840fae95f22ab83c4a0e024e5e1a64d8ff67
Stanimir Venkov <stanimir.venkov@eldersoss.com>
2023-01-10 15:55:37 +0200

    Solve task 3 part 2

--- DayThree/main.go
+++ DayThree/main.go
@@ -6,31 +6,61 @@ import (
 )
 
 func main() {
-	fmt.Println("test")
-	lines := filereader.ReadFile("input.txt")
-	counter := 0
+	input := filereader.ReadFile("input.txt")
 
-	for _, line := range lines {
-		counter += getRepeatedItemNumber(line)
-	}
+	counter := findTotalPriority(input)
+	badgesPriority := findBadgesTotalPriority(input)
 
-	fmt.Println("==========================")
-	fmt.Print("Final Answer is: ")
 	fmt.Println(counter)
+	fmt.Println(badgesPriority)
 }
 
-func getRepeatedItemNumber(line string) int {
-	length := len(line)
+func findTotalPriority(lines []string) int {
 
-	for i := 0; i < 3; i++ {
-		fmt.Println(i)
+	counter := 0
+	for _, line := range lines {
+		counter += getPriorityOfItemThatRepeats(line)
 	}
+
+	return counter
+}
+
+func findBadgesTotalPriority(lines []string) int {
+	counter := 0
+	counterggg := 0
+	increaseWith := 3
+	for i := 0; i < len(lines); i += increaseWith {
+		value := getGroupBadge(lines, i)
+		counterggg++
+		// fmt.Println("counter -> ", counterggg, " and with common value -> ", value, "and i = ", i)
+		counter += value
+	}
+
+	return counter
+}
+
+func getGroupBadge(lines []string, sentenceNumber int) int {
+	for j, letter := range lines[sentenceNumber] {
+		for _, letter2 := range lines[sentenceNumber+1] {
+			for _, letter3 := range lines[sentenceNumber+2] {
+				if letter == letter2 && letter == letter3 {
+					return getNumberOfItem(lines[sentenceNumber][j])
+				}
+			}
+		}
+	}
+
+	return 0
+}
+
+func getPriorityOfItemThatRepeats(line string) int {
+	length := len(line)
 
 	for i := 0; i < length/2; i++ {
 		for j := length / 2; j < length; j++ {
 			if line[i] == line[j] {
 				repeatedItemNumber := getNumberOfItem(line[i])
-				fmt.Printf("%v -> %v \n", line[i], repeatedItemNumber)
+				//fmt.Printf("%v -> %v \n", line[i], repeatedItemNumber)
 
 				return repeatedItemNumber
 			}
@@ -49,6 +79,3 @@ func getNumberOfItem(letter byte) int {
 		return codeInAscii - 96
 	}
 }
-
-// nhgPFmsnLPGLhPDJhG
-// TcDjMfrMMjMZWfjfWj
